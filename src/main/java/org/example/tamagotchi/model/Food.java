a3ba546921fd907b83565d3b9f3f1e4c5da1688d
maugalcst <maugal.cst@gmail.com>
2025-01-22 14:08:45 -0600

    add Food attributes, constructor and getters

--- src/main/java/org/example/tamagotchi/model/Food.java
+++ src/main/java/org/example/tamagotchi/model/Food.java
@@ -1,4 +1,29 @@
 package org.example.tamagotchi.model;
 
 public class Food {
+
+    private String nombre;
+    private int valorNutricional;
+    private String tipo;
+
+    public Food(String nombre, int valorNutricional, String tipo) {
+        this.nombre = nombre;
+        this.valorNutricional = valorNutricional;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getValorNutricional() {
+        return valorNutricional;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    
+
 }
